6c18834c8e3febf92f47b79b8fda666d28e0d9bf
Matheus Santana <santalvesmatheus@gmail.com>
2023-11-21 16:58:32 -0300

    initial commit

---
+++ sumDivisibleNumbers.js
@@ -0,0 +1,7 @@
+function sumDivisibleNumbers(num) {
+  return Array.from({ length: num - 1 }, (_, i) => i + 1)
+    .filter((n) => n % 3 === 0 || n % 5 === 0)
+    .reduce((acc, curr) => acc + curr, 0);
+}
+
+console.log(sumDivisibleNumbers(10))
